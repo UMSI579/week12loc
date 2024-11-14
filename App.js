c4147724d92bfab4c4ab0a37915cf9abf65ddd46
Mullins, Benjamin <benm@umich.edu>
2024-11-13 19:36:46 -0500

    search

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
-
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import { GOOGLE_API_KEY } from './Secrets';
+
 import {
   Accuracy,
   requestForegroundPermissionsAsync,
@@ -9,14 +11,23 @@ import {
 
 export default function App() {
 
+  const [ places, setPlaces ] = useState([]);
+
+  const initRegion = {
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  }
   const [location, setLocation] = useState(null);
   const [ permissionsGranted, setPermissionsGranted ] = useState(false);
+  const [ mapRegion, setMapRegion ] = useState(initRegion);
+
 
   let unsubscribeFromLocation = null;
 
   const subscribeToLocation = async () => {
-
-    let { status } = await requestForegroundPermissionsAsync();
+    const { status } = await requestForegroundPermissionsAsync();
     setPermissionsGranted(status === 'granted');
 
     if (unsubscribeFromLocation) {
@@ -27,14 +38,42 @@ export default function App() {
       distanceInterval: 1, // 1 meter
       timeInterval: 1000 // 1000ms = 1s
     }, location => {
-      console.log('received update:', location);
       setLocation(location);
+      setMapRegion({
+        ...mapRegion,
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude
+      })
     });
   }
 
   useEffect(() => {
     subscribeToLocation();
   }, []);
+
+  const searchLocations = async () => {
+    const params = new URLSearchParams({
+      location: `${location.coords.latitude},${location.coords.longitude}`,
+      type: 'restaurant',
+      radius: '5000', // 5KM
+      key: GOOGLE_API_KEY
+    });
+
+    const placesURI = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?${params.toString()}`
+    const response = await fetch(placesURI);
+    const results = await response.json();
+    const newPlaces = [];
+    for (let r of results.results) {
+      const newPlace = {};
+      newPlace.latitude = r.geometry.location.lat;
+      newPlace.longitude = r.geometry.location.lng;
+      newPlace.name = r.name;
+      newPlace.id = r.place_id;
+      newPlaces.push(newPlace);
+    }
+    setPlaces(newPlaces);
+  }
+
 
   return (
     <View style={styles.container}>
@@ -49,6 +88,33 @@ export default function App() {
           "Location permission not granted."
         }
       </Text>
+
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        region={mapRegion}
+        initialRegion={initRegion}
+        showsUserLocation={true}
+      >
+
+        {places.map(place => {
+          return (
+            <Marker
+              key={place.id}
+              coordinate={{
+                latitude: place.latitude,
+                longitude: place.longitude
+              }}
+              title={place.name}
+            />
+          )
+        })}
+      </MapView>
+
+      <Button
+        title="Search for Restaurants"
+        onPress={searchLocations}
+      />
     </View>
   );
 }
@@ -62,4 +128,9 @@ const styles = StyleSheet.create({
   paragraph: {
     fontSize: 24
   },
+  map: {
+    flex: 0.5,
+    width: '100%',
+    height: '100%'
+  }
 })
